0785f1add6d4ce7814f7db84b99c21a15e7af978
unknown <prasannavenkateshrao@gmail.com>
2021-11-25 23:30:51 +0530

    Updated test cases for all node API's

--- chatbot-server/tests/BotDataServiceTest.js
+++ chatbot-server/tests/BotDataServiceTest.js
@@ -19,6 +19,9 @@ var expectedChatBotResponse={
     "conversationValue": "First Name",
     "visitorAuthResponse": "67bb469433cb732caa7a:d573dad1ee48a81f56dd5803419c3424ad53305a92764cbde2f19b033f6186a4"
 };
+var updateAPIResponse = {
+    "updateStatus": "success"
+};
 describe("Bot data service test", function() {
 describe("check get chatbot", function() {
     var url = "http://localhost:8000/get/chat-bot";
@@ -33,6 +36,30 @@ describe("check get chatbot", function() {
     it("verify response is getting returned as expected", function() {
         requestPromise(url, function(error, response, body) {
         expect(response.body).to.equal(expectedChatBotResponse);
+      });
+    });
+  });
+  describe("check updateTimeStamp function", function() {
+    var url = "http://localhost:8000/update/user/conversation?operation=spentTime&eventType=USER_DATA_PROVIDED&timestamp=1637573502442&field=phone&timeSpent=2";
+    it("verify response is getting returned as expected", function() {
+        requestPromise(url, function(error, response, body) {
+        expect(response.body).to.equal(spentTimeResponse);
+      });
+    });
+  });
+  describe("check read API function", function() {
+    var url = "http://localhost:8000/update/user/conversation?operation=read";
+    it("verify response is getting returned as expected", function() {
+        requestPromise(url, function(error, response, body) {
+        expect(response.body).to.equal(updateAPIResponse);
+      });
+    });
+  });
+  describe("deliver API function", function() {
+    var url = "http://localhost:8000/update/user/conversation?operation=deliver";
+    it("verify response is getting returned as expected", function() {
+        requestPromise(url, function(error, response, body) {
+        expect(response.body).to.equal(updateAPIResponse);
       });
     });
   });
